9c5fbeeb8f2e19afa5313d5ffa2f71395213eedc
Joey Groves <link_groves@yahoo.co.uk>
2024-07-12 17:01:34 +0100

    added colour pallete in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ export default {
   theme: {
     extend: {
       colors: {
-
+        blue: "#137EFB",
+        grey: "#767676",
+        "deep-blue": "#2D2E32",
+        "light-grey": "#F9F9F9"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"]
