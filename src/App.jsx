e4dfaf5d370f98a88a5a07770c5a238360f02379
Joey Groves <link_groves@yahoo.co.uk>
2024-07-12 18:29:40 +0100

    passing props to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="app">
       <NavBar 
-        
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
     </div>
   )
