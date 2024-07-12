b2cf081797ed73c4317dcbd3833a0f214a6310d8
Joey Groves <link_groves@yahoo.co.uk>
2024-07-12 19:10:08 +0100

    added isMenuToggled state for NavBar to add support for mobile devices

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 
-const NavBar = () => {
+const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+
     return (
         <p>Hello World!</p>
     )
