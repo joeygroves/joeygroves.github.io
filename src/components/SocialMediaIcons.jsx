ceaf478d1457bad95541f382301e3bed4adf0f8f
Joey Groves <link_groves@yahoo.co.uk>
2024-07-13 20:13:13 +0100

    added linkedin icon and link

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,7 +1,19 @@
 const SocialMediaIcons = () => {
     return (
-        <></>
-    )
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com/in/joewesleygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/linkedin.svg"
+                    alt="linkedin-link"
+                />
+            </a>
+        </div>
+    );
 };
 
 export default SocialMediaIcons;
