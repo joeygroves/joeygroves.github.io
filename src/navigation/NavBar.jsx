4f50c3b78c54664cd511823dc0ce964a8d1a8848
Joey Groves <link_groves@yahoo.co.uk>
2024-07-13 03:52:35 +0100

    changed colour and size of menu-close-icon

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -67,7 +67,9 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                     <div className="fixed right-0 bottom-0 h-full bg-light-grey w-[250px] shadow-md">
                         {/* Close Icon */}
                         <div className="flex justify-end p-12">
-                            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <button 
+                                onClick={() => setIsMenuToggled(!isMenuToggled)}
+                            >
                                 <img src="../src/assets/menu-close-icon.svg" alt="menu-close-icon" />
                             </button>
                         </div>
