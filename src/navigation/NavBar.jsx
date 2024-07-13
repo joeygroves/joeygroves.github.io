d2c399272b8b93c33c6f71e23550acb2f0da1e66
Joey Groves <link_groves@yahoo.co.uk>
2024-07-13 01:54:49 +0100

    changed navbar text size

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     return (
         <nav className="z-40 w-full fixed top-0 py-6 shadow-md">
             <div className="flex items-center justify-between mx-auto w-5/6">
-                <h4 className="font-poppins text-3xl text-deep-blue font-bold">Joe Groves</h4>
+                <h4 className="font-poppins text-2xl text-deep-blue font-bold">Joe Groves</h4>
             </div>
         </nav>
     )
