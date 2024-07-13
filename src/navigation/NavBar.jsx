7eaffcd821d18a04a4b34d2cb3bd8ab7c7f85a1c
Joey Groves <link_groves@yahoo.co.uk>
2024-07-13 02:59:37 +0100

    installed and imported react-anchor-link-smooth-scroll package

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+    const lowerCasePage = page.toLowerCase();
+    return (
+      <AnchorLink
+        className={`${selectedPage === lowerCasePage ? "text-deep-blue" : ""}
+                  hover:text-blue transition duration-500`}
+        href={`#${lowerCasePage}`}
+        onClick={() => setSelectedPage(lowerCasePage)}
+      >
+        {page}
+      </AnchorLink>
+    );
+  };
 
 const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
@@ -11,7 +26,7 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                 <h4 className="font-poppins text-2xl text-deep-blue font-bold">Joe Groves</h4>
                 {/* DESKTOP NAV*/}
                 {isAboveSmallScreens ? (
-                    <div className="flex justify-between gap-16 font-poppins text-sm font-semibold text-deep-blue">
+                    <div className="flex justify-between gap-16 font-poppins text-sm font-semibold">
                         <Link
                             page="Home"
                             selectedPage={selectedPage}
