20d0a01c1ebf256490c0296433e11f8f0887fa80
Joey Groves <link_groves@yahoo.co.uk>
2024-07-13 01:51:00 +0100

    added box shadow to navbar

--- src/navigation/NavBar.jsx
+++ src/navigation/NavBar.jsx
@@ -4,9 +4,9 @@ const NavBar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
 
     return (
-        <nav className="z-40 w-full fixed top-0 py-6">
+        <nav className="z-40 w-full fixed top-0 py-6 shadow-md">
             <div className="flex items-center justify-between mx-auto w-5/6">
-                <h4 className="font-poppins text-3xl font-bold">Joe Groves</h4>
+                <h4 className="font-poppins text-3xl text-deep-blue font-bold">Joe Groves</h4>
             </div>
         </nav>
     )
