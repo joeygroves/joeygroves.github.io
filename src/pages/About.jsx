5cd631170cfbdebf3121c5e141fe9a6f73ea489e
Joey Groves <link_groves@yahoo.co.uk>
2024-07-14 17:31:16 +0100

    imported useMediaQuery hook in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,8 @@
+import useMediaQuery from '../hooks/useMediaQuery';
+
 const About = () => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    
     return (
         <div>
             <h3>Hello World</h3>
