c34b26ffc91b59e433fc3038d356f52999034bc6
Joey Groves <link_groves@yahoo.co.uk>
2024-07-14 19:03:01 +0100

    added svg icons to tech stack icons component

--- src/components/TechStackIcons.jsx
+++ src/components/TechStackIcons.jsx
@@ -1,94 +1,38 @@
 const TechStackIcons = () => {
     return (
         <div className="flex justify-center md:justify-start my-7 gap-3">
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://linkedin.com/in/joewesleygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/linkedin.svg"
-                    alt="linkedin-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://github.com/joeygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/github.svg"
-                    alt="github-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://linkedin.com/in/joewesleygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/linkedin.svg"
-                    alt="linkedin-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://github.com/joeygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/github.svg"
-                    alt="github-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://linkedin.com/in/joewesleygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/linkedin.svg"
-                    alt="linkedin-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://github.com/joeygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/github.svg"
-                    alt="github-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://linkedin.com/in/joewesleygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/linkedin.svg"
-                    alt="linkedin-link"
-                />
-            </a>
-            <a
-                className="hover:opacity-50 transition duration-500"
-                href="https://github.com/joeygroves"
-                target="_blank"
-                rel="noreferrer"
-            >
-                <img 
-                    src="../src/assets/github.svg"
-                    alt="github-link"
-                />
-            </a>
+            <img 
+                src="../src/assets/html.svg"
+                alt="html"
+            />
+            <img 
+                src="../src/assets/css.svg"
+                alt="css"
+            />
+            <img 
+                src="../src/assets/javascript.svg"
+                alt="javascript"
+            />
+            <img 
+                src="../src/assets/react.svg"
+                alt="react"
+            />
+            <img 
+                src="../src/assets/nodejs.svg"
+                alt="node js"
+            />  
+            <img 
+                src="../src/assets/mongodb.svg"
+                alt="mongoDB"
+            />
+            <img 
+                src="../src/assets/tailwind.svg"
+                alt="tailwind css"
+            />
+            <img 
+                src="../src/assets/python.svg"
+                alt="python"
+            />         
         </div>
     );
 }
