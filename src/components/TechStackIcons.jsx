61539053b97a8b9c51b9c0b17f09ab83826e4944
Joey Groves <link_groves@yahoo.co.uk>
2024-07-14 19:23:54 +0100

    added text under icons of tech stack component

--- src/components/TechStackIcons.jsx
+++ src/components/TechStackIcons.jsx
@@ -1,38 +1,70 @@
 const TechStackIcons = () => {
     return (
-        <div className="flex justify-center md:justify-start my-7 gap-3">
-            <img 
-                src="../src/assets/html.svg"
-                alt="html"
-            />
-            <img 
-                src="../src/assets/css.svg"
-                alt="css"
-            />
-            <img 
-                src="../src/assets/javascript.svg"
-                alt="javascript"
-            />
-            <img 
-                src="../src/assets/react.svg"
-                alt="react"
-            />
-            <img 
-                src="../src/assets/nodejs.svg"
-                alt="node js"
-            />  
-            <img 
-                src="../src/assets/mongodb.svg"
-                alt="mongoDB"
-            />
-            <img 
-                src="../src/assets/tailwind.svg"
-                alt="tailwind css"
-            />
-            <img 
-                src="../src/assets/python.svg"
-                alt="python"
-            />         
+        <div className="flex justify-center md:justify-start my-7 gap-5">
+            <div>
+                <img 
+                    src="../src/assets/html.svg"
+                    alt="html"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">HTML</p>
+            </div>
+
+            <div>
+                <img 
+                    src="../src/assets/css.svg"
+                    alt="css"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">CSS</p>
+            </div>
+
+            <div>
+                <img 
+                    src="../src/assets/javascript.svg"
+                    alt="javascript"
+                    className="justify-center"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">JavaScript</p>
+            </div>
+
+            <div>
+                <img 
+                    src="../src/assets/react.svg"
+                    alt="react"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">React</p>
+            </div>
+
+            <div>
+                <img 
+                    src="../src/assets/nodejs.svg"
+                    alt="node js"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">Node.js</p>
+            </div>
+
+            <div>
+                <img 
+                    src="../src/assets/mongodb.svg"
+                    alt="mongodb"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">MongoDB</p>
+            </div>
+
+            <div>
+                <img 
+                    src="../src/assets/tailwind.svg"
+                    alt="tailwind css"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">TailwindCSS</p>
+            </div>
+            
+            <div>
+                <img 
+                    src="../src/assets/python.svg"
+                    alt="python"
+                />
+                <p className="mt-1 text-center font-poppins font-light text-grey text-sm">Python</p>
+            </div>        
         </div>
     );
 }
