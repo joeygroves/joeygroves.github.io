b8210d02972215800937e225d5d21095ce929d39
Joey Groves <link_groves@yahoo.co.uk>
2024-07-14 22:46:31 +0100

    changed single quotations to double quotations

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,9 +2,17 @@ const Projects = () => {
     return(
         <section
             id="projects"
-            className=""
+            className="pt-48 pb-48"
         >
-            <p>Hello World!</p>
+            <div>
+                <div>
+                    <p className="font-poppins font-semibold text-3xl mb-5 text-blue text-center">PROJECTS</p>
+                </div>
+            </div>
+
+            <div className="flex justify-center">
+                
+            </div>
         </section>
     )
 
