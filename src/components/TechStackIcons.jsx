ce20c55894a7527dd2ee3b36847d260d4b76b6a6
Joey Groves <link_groves@yahoo.co.uk>
2024-07-14 18:31:06 +0100

    created template for tech stack icons

---
+++ src/components/TechStackIcons.jsx
@@ -0,0 +1,96 @@
+const TechStackIcons = () => {
+    return (
+        <div className="flex justify-center md:justify-start my-7 gap-3">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://linkedin.com/in/joewesleygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/linkedin.svg"
+                    alt="linkedin-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://github.com/joeygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/github.svg"
+                    alt="github-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://linkedin.com/in/joewesleygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/linkedin.svg"
+                    alt="linkedin-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://github.com/joeygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/github.svg"
+                    alt="github-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://linkedin.com/in/joewesleygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/linkedin.svg"
+                    alt="linkedin-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://github.com/joeygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/github.svg"
+                    alt="github-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://linkedin.com/in/joewesleygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/linkedin.svg"
+                    alt="linkedin-link"
+                />
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://github.com/joeygroves"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img 
+                    src="../src/assets/github.svg"
+                    alt="github-link"
+                />
+            </a>
+        </div>
+    );
+}
+
+export default TechStackIcons;
