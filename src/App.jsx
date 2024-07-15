9f38e5598b62336c49642d731832d00079679cc7
Joey Groves <link_groves@yahoo.co.uk>
2024-07-15 18:55:59 +0100

    added isTopOfPage props into NavBar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
   return (
     <div className="app">
       <NavBar 
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
