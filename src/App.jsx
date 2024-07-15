1ad51741bf424471078e8c6fe1d8d6347ff975f6
Joey Groves <link_groves@yahoo.co.uk>
2024-07-15 18:54:41 +0100

    added useEffect to check if currently on top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,15 @@ function App() {
   const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  })
+
   return (
     <div className="app">
       <NavBar 
