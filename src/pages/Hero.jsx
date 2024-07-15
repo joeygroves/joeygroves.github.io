4d3e9d07ac2e68de0514e3cc696d45e6e608b70d
Joey Groves <link_groves@yahoo.co.uk>
2024-07-15 19:41:03 +0100

    changed delay of framer-motion of social media icons in hero section from 0.4 to 0.2

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
                     initial="hidden"
                     whileInView="visible"
                     viewport={{ once: true, amount: 0.5 }}
-                    transition={{ delay: 0.4, duration: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
                     variants={{
                       hidden: { opacity: 0, x: -50 },
                       visible: { opacity: 1, x: 0 },
