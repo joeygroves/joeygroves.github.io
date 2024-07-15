1d5860665fdcb9bba626383d20e1b87e9fc35e9c
Joey Groves <link_groves@yahoo.co.uk>
2024-07-15 18:40:04 +0100

    added framer-motion to social media icons

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -48,9 +48,18 @@ const Hero = ({ setSelectedPage }) => {
                     </p>
                 </motion.div>
 
-                <div>
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}
+                    variants={{
+                      hidden: { opacity: 0, x: -50 },
+                      visible: { opacity: 1, x: 0 },
+                    }}
+                >
                     <SocialMediaIcons />
-                </div>
+                </motion.div>
             </div>
         </section>
     )
