986ce1243059e7007feff2106f64b25cac3cf6fb
Joey Groves <link_groves@yahoo.co.uk>
2024-07-15 02:49:44 +0100

    created and styled project container

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,13 +6,51 @@ const Projects = () => {
         >
             <div>
                 <div>
-                    <p className="font-poppins font-semibold text-3xl mb-5 text-blue text-center">PROJECTS</p>
+                    <p className="font-poppins font-semibold text-3xl mb-10 text-blue text-center">PROJECTS</p>
                 </div>
             </div>
 
-            <div className="flex justify-center">
-                
+            <div className="flex flex-col justify-between items-center mx-8 py-9">
+                <div className="md:flex">
+                    <div>
+                        <p className="font-poppins font-semibold text-xl text-deep-blue">To-Do List App</p>
+                        <p className="font-poppins text-md text-grey mb-4">April - May 2024</p>
+
+                        <p className="font-poppins text-deep-blue mb-2">A 10-Week part-time product marketing internship with GlowTouch. 
+                            Redesigned the homepage of SaaS product HiveDesk, aiming to direct user flow 
+                            and increase CTR from homepage to free trial download page.
+                        </p>
+
+                        <button 
+                            className="my-4 bg-transparent hover:bg-blue text-deep-blue font-semibold 
+                            hover:text-white transition duration-500 py-2 px-4 border border-deep-blue hover:border-transparent rounded"
+                        >
+                            View on GitHub
+                        </button>
+                        
+                        <div className="md:mt-0 mt-5 mb-6">
+                            <div className="flex">
+                                <p className="font-poppins text-sm text-deep-blue font-semibold mb-2 mr-8">Technologies</p>
+                                <p className="font-poppins text-sm text-deep-blue mb-2">React, Express, Node.js, MongoDB, Tailwind CSS, Figma</p>
+                            </div>
+                            <div className="flex">
+                                <p className="font-poppins text-sm text-deep-blue font-semibold mb-2 mr-8">Skills</p>
+                                <p className="font-poppins text-sm text-deep-blue mb-2">Front-End, Back-End, Database Management, UX Design</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img
+                            src="../src/assets/to-do-app.png"
+                            alt="to-do app"
+                            className="md:max-w-[600px] md:ml-10"
+                        />
+                    </div>
+                </div>
             </div>
+
+
         </section>
     )
 
