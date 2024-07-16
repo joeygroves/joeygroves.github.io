96ad41bedf7d6bf3d7c9c3419c0655a461cce69d
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 18:58:17 +0100

    seperated footer information for flex

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,15 @@ import SocialMediaIcons from "./SocialMediaIcons";
 
 const Footer = () => {
     return (
-        <footer className="h-48 bg-deep-blue pt-10">
+        <footer className="h-56 bg-deep-blue pt-10">
             <div className="w-5/6 mx-auto">
             <p className="font-poppins font-black text-3xl text-white mb-3 md:justify-start md:text-left text-center">Joe Groves</p>
-                <div className="md flex justify-center md:justify-start text-center mt-2">
+                <div className="md:flex justify-center md:justify-start text-center mt-2">
                     <p className="font-poppins font-normal text-md text-white">
-                        Copyright © 2024 Joe Groves<span className="mx-1">|</span> Coded + Designed on <a href="https://www.github.com/joeygroves" className="text-light-grey underline hover:text-blue transition duration-500">joeygroves</a>. いらっしゃいませ ✌🏼
+                        Copyright © 2024 Joe Groves <span className="mx-1">|</span> 
                     </p>
+                    <p className="ml-1 font-poppins font-normal text-md text-white">Coded + Designed on <a href="https://www.github.com/joeygroves" className="text-light-grey underline hover:text-blue transition duration-500">joeygroves</a>.</p>
+                    <p className="ml-1 font-poppins font-normal text-md text-white">いらっしゃいませ ✌🏼</p>
                 </div>
             </div>
         </footer>
