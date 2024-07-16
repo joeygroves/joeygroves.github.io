e9d7b640b091853ada4034c2e1612700ffb379cc
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 19:23:40 +0100

    added white social media icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,38 @@ import SocialMediaIcons from "./SocialMediaIcons";
 
 const Footer = () => {
     return (
-        <footer className="h-52 bg-deep-blue pt-10">
+        <footer className="h-64 bg-deep-blue pt-10">
             <div className="w-5/6 mx-auto">
-                <p className="font-poppins font-black text-3xl text-white mb-3 md:justify-start md:text-left text-center md:mt-8">Joe Groves</p>
+                <p className="font-poppins font-black text-3xl text-white mb-3 md:justify-start md:text-left text-center md:mt-6">Joe Groves</p>
+
+                {/* White Social Media Icons */}
+                <div>
+                    <div className="flex justify-center md:justify-start my-3 gap-3">
+                        <a
+                            className="hover:opacity-50 transition duration-500"
+                            href="https://linkedin.com/in/joewesleygroves"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <img 
+                                src="../src/assets/contact-linkedin.svg"
+                                alt="linkedin-link"
+                            />
+                        </a>
+                        <a
+                            className="hover:opacity-50 transition duration-500"
+                            href="https://github.com/joeygroves"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <img 
+                                src="../src/assets/contact-github.svg"
+                                alt="github-link"
+                            />
+                        </a>
+                    </div>
+                </div>
+
                 <div className="md:flex justify-center md:justify-start text-center mt-2">
                     <p className="font-poppins font-normal text-md text-white">
                         Copyright © 2024 Joe Groves <span className="mx-1">|</span> 
