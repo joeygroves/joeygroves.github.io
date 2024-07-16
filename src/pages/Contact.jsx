40ec73ae0273adfc4b2eed0bbf0f50ffb174e315
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 17:00:15 +0100

    added section tag and test text

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,14 @@
 import { motion } from "framer-motion"; 
 
 const Contact = () => {
-
+    return (
+        <section
+            id="contact"
+            className="pt-48 pb-48"
+        >
+            <p>Hello World!</p>
+        </section>
+    )
 }
 
 export default Contact;
