637b27fe191e71ea6ce3280deec0200bea013f7a
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 14:39:42 +0100

    added headings to etc. page

--- src/pages/Etc.jsx
+++ src/pages/Etc.jsx
@@ -4,7 +4,14 @@ const Etc = () => {
             id="etc."
             className="pt-48 pb-48"
         >
-            <p>Hello World!</p>
+            <div>
+                <div>
+                    <p className="font-poppins font-semibold text-3xl text-blue text-center">ET CETERA</p>
+                    <p className="my-2 text-sm text-center text-grey font-poppins font-light">
+                        INTERESTED IN MORE WORK + ART?
+                    </p>
+                </div>
+            </div>
         </section>
     )
 
