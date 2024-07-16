7b19321cf5bbef3ade39563d85896dd442395299
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 19:12:06 +0100

    styled footer information for medium screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import SocialMediaIcons from "./SocialMediaIcons";
 
 const Footer = () => {
     return (
-        <footer className="h-56 bg-deep-blue pt-10">
+        <footer className="h-52 bg-deep-blue pt-10">
             <div className="w-5/6 mx-auto">
-            <p className="font-poppins font-black text-3xl text-white mb-3 md:justify-start md:text-left text-center">Joe Groves</p>
+                <p className="font-poppins font-black text-3xl text-white mb-3 md:justify-start md:text-left text-center md:mt-8">Joe Groves</p>
                 <div className="md:flex justify-center md:justify-start text-center mt-2">
                     <p className="font-poppins font-normal text-md text-white">
                         Copyright © 2024 Joe Groves <span className="mx-1">|</span> 
