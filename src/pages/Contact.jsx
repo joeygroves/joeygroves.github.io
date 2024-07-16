7777160b9b11bef6245378342d7a4421f886b609
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 17:20:37 +0100

    added sub-heading and animation to sub-heading

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,35 @@ const Contact = () => {
             id="contact"
             className="pt-48 pb-48"
         >
-            <p>Hello World!</p>
+            {/* Heading */}
+            <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                      hidden: { opacity: 0, x: -50 },
+                      visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <p className="font-poppins font-semibold text-3xl mb-5 text-blue">
+                        CONTACT
+                    </p>
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay:0.2, duration: 0.5 }}
+                        variants={{
+                          hidden: { opacity: 0, x: -50 },
+                          visible: { opacity: 1, x: 0 },
+                        }}
+                    >
+                        <p className="font-poppins text-grey mb-7">
+                            Don't be shy! Let's get in touch if you have a project / collaboration in mind. Or if you just want a coffee chat. 😁
+                        </p>
+                    </motion.div>
+            </motion.div>
         </section>
     )
 }
