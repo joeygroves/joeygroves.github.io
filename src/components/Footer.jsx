29b8262e9840ce5fe39c8ef6fb1da87325108248
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 18:53:26 +0100

    added copyright and correct information in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,13 @@ import SocialMediaIcons from "./SocialMediaIcons";
 
 const Footer = () => {
     return (
-        <footer className="h-64 bg-white pt-10">
+        <footer className="h-48 bg-deep-blue pt-10">
             <div className="w-5/6 mx-auto">
-            <p className="font-montserrat font-black text-3xl text-deep-blue mb-3 md:justify-start md:text-left text-center">Joe Groves</p>
-                <SocialMediaIcons />
-                <div className="md flex justify-center md:justify-end text-center mt-2">
-                    <p className="font-montserrat font-normal text-md text-deep-blue">© 2024 ELLIE SMITH | Designed & Developed by <a href="https://www.github.com/joeygroves" className="hover:text-black transition duration-500">Joey Groves</a></p>
+            <p className="font-poppins font-black text-3xl text-white mb-3 md:justify-start md:text-left text-center">Joe Groves</p>
+                <div className="md flex justify-center md:justify-start text-center mt-2">
+                    <p className="font-poppins font-normal text-md text-white">
+                        Copyright © 2024 Joe Groves<span className="mx-1">|</span> Coded + Designed on <a href="https://www.github.com/joeygroves" className="text-light-grey underline hover:text-blue transition duration-500">joeygroves</a>. いらっしゃいませ ✌🏼
+                    </p>
                 </div>
             </div>
         </footer>
