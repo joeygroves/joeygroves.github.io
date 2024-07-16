2caa7d020ce827dd3cc36a5b1319b5145c6b7914
Joey Groves <link_groves@yahoo.co.uk>
2024-07-16 14:28:51 +0100

    added section tag and test text in Etc page

--- src/pages/Etc.jsx
+++ src/pages/Etc.jsx
@@ -1,4 +1,12 @@
 const Etc = () => {
+    return (
+        <section 
+            id="etc"
+            className="pt-48 pb-48"
+        >
+            <p>Hello World!</p>
+        </section>
+    )
 
 }
 
