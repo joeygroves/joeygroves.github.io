9068c568c84b2462579f923947999947d943591e
Joey Groves <link_groves@yahoo.co.uk>
2024-07-17 04:23:27 +0100

    added base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/joeygroves.github.io",
   css: {
     postcss: {
       plugins: [tailwindcss],
