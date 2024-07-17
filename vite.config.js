ef25cefb57e0ca5370847b384ad662b8ba2939f1
Joey Groves <link_groves@yahoo.co.uk>
2024-07-17 04:43:23 +0100

    changed base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/joeygroves.github.io",
+  base: "/",
   css: {
     postcss: {
       plugins: [tailwindcss],
